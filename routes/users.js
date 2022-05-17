2a3a7fe0ea33258f740b40e4efffffb5f0636721
Steven Moyano <stevenmoyano1@gmail.com>
2022-05-17 13:05:48 -0500

    completed required tasks

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const favMovies = ["Godfather", "Avatar", "Scarface"];
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/myname", (req, res) => {
+  res.send("Steven");
+});
+
+router.get("/myfavoritemovies", (req, res) => {
+  res.json(favMovies);
 });
 
 module.exports = router;
